ac6114eb3fc8c6e794469589dc175bfeffd51a3a
Long Trinh <trinhlong1749@gmail.com>
2022-06-14 22:26:34 +0700

    script work submit

--- homewerk/seeds/data/7-Submit.sql
+++ homewerk/seeds/data/7-Submit.sql
@@ -1,2 +1,2 @@
 insert into `submits` (assignment_id, user_id, status, result, id, comment) values
-()
+(1, 1, 'graded', 6, 1, 'Cần nhiều cố gắng hơn');
--- homewerk/seeds/data/8-Work.sql
+++ homewerk/seeds/data/8-Work.sql
@@ -1,1 +1,4 @@
-insert into `work` (`submit_id`, `image_path`, `priority`, `canvas_json`, `id`, `active`, `result_path`) values
+insert into `works` (`submit_id`, `image_path`, `priority`, `canvas_json`, `id`, `active`, `result_path`) values
+(1, 'https://storage.googleapis.com/hoemwerk-bucket/image/submit/89c61a72-68b1-4abb-82b1-616a399eaff1', 1, '{"version":"5.2.1","objects":[]}', 1, 1,'https://storage.googleapis.com/hoemwerk-bucket/image/submit/ab2ceaf2-86c5-4ca5-8304-b1e440fa9f59'),
+(1, 'https://storage.googleapis.com/hoemwerk-bucket/image/submit/b7d1539a-f996-4f91-b1e0-1afe11924ce1', 2, '{"version":"5.2.1","objects":[]}', 2, 1,'https://storage.googleapis.com/hoemwerk-bucket/image/submit/0c051e68-0f40-491d-bb52-538dc5c670fd'),
+(1, 'https://storage.googleapis.com/hoemwerk-bucket/image/submit/a22e6ac4-6747-49cb-86b4-63dbec9b8ef3', 3, '{"version":"5.2.1","objects":[]}', 3, 1,'https://storage.googleapis.com/hoemwerk-bucket/image/submit/9c1b68c5-dc98-469b-8195-0edad4324352');
