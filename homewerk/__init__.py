095ab9d1a0b1fdd6ea991b3930063cef1f719006
Long Trinh <trinhlong1749@gmail.com>
2022-08-13 11:27:32 +0700

    update sql uri

--- homewerk/__init__.py
+++ homewerk/__init__.py
@@ -14,7 +14,8 @@ def create_app():
     app = flask.Flask(__name__)
     load_app_config(app)
     app.config['SECRET_KEY'] = 'hardsecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/homewerk'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/homewerk'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dswowingafuocg:c5e2549703171160d56c5f4303bf651735c3db6d0c3da7fb16034a72f5f2dd14@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d72jt3kt6cmltb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['CORS_SUPPORTS_CREDENTIALS'] = True
     app.config['CORS_HEADERS'] = 'Content-Type'
